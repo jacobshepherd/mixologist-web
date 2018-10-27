f7c48af8903375d62a3b9a61ba5fb55e27abfaf5
Michael Stark <mstark5652@gmail.com>
2018-10-26 22:26:46 -0500

    contact email

--- php/contact.php
+++ php/contact.php
@@ -22,14 +22,14 @@ $msg   = $_REQUEST["message"];
 // $website = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
 // $website = dirname($website);
 // $website = dirname($website);
-$website = "https://www.mixologist.ai";
+// $website = "https://www.mixologist.ai";
 
 if (isset($email) && isset($name)) {
 
 	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+	$headers .= "Content-type:text/html;charset=utf-8" . "\r\n";
 	$headers .= "From: hello@mixologist.ai"."\r\n"."Reply-To: ".$email."\r\n" ;
-	$emsg     = "Hello,<br/><br/> You have received a message from your website contact form. Here are the details. <br/><br/> From: $name<br/> Email: $email <br/>Message: $msg <br><br> -- <br>This e-mail was sent from a contact form on $website";
+	$emsg     = "Hello,<br/><br/> You have received a message from your website contact form. Here are the details. <br/><br/> From: $name<br/> Email: $email <br/>Message: $msg <br><br> -- <br>This e-mail was sent from a contact form on https://www.mixologist.ai.";
 	
     $mailr =  mail($to, $subject, $emsg, $headers);
 	if($mailr)
