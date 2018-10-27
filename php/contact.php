dfcd35dd3288e72dea9797d819443e0ad08f2e9e
Michael Stark <mstark5652@gmail.com>
2018-10-27 11:22:43 -0500

    contact php to use cURL

--- php/contact.php
+++ php/contact.php
@@ -22,9 +22,27 @@ $msg   = $_REQUEST["message"];
 $apiKey = getenv("AWS_LAMBDA_KEY");
 $apiEndpoint = "https://36p03itvai.execute-api.us-east-2.amazonaws.com/default/contactSubmission?platform=mixologist&name=$name&email=$email&subject=$subject&message=$msg";
 
+//Initialize cURL.
+$ch = curl_init();
 // add header for api key
-$res = http_get($apiEndpoint);
-if ($res) {
+curl_setopt($ch, CURLOPT_URL, $apiEndpoint);
+//Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+// add headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	"x-api-key: $apiKey",
+	"Accept: */*"
+));
+
+//Execute the request.
+$data = curl_exec($ch);
+//Close the cURL handle.
+curl_close($ch);
+    
+if ($data) {
 	echo 'success';
 } else {
 	echo 'failure';
