adae36f9fa426dd6aa34e2e4a4bdf9e17543cf3e
Michael Stark <mstark5652@gmail.com>
2018-10-27 20:58:48 -0500

    contact logging

--- php/contact.php
+++ php/contact.php
@@ -9,7 +9,7 @@
 	$apiEndpoint = "https://36p03itvai.execute-api.us-east-2.amazonaws.com/default/contactSubmission?service=mixologist&name=$name&email=$email&subject=$subject&message=$msg";
 	
 	$encoded = rawurlencode($apiEndpoint);
-	$encoded = str_replace(" ", "%20", $encoded);
+	$encoded = str_replace(' ', "%20", $encoded);
 	
 	//Initialize cURL.
 	$ch = curl_init();
