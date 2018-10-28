4ef1881f9084805f9eb0f1e0a469ba6d97ffecbd
Michael Stark <mstark5652@gmail.com>
2018-10-27 21:11:04 -0500

    contact form submission

--- php/contact.php
+++ php/contact.php
@@ -6,11 +6,9 @@
 	$msg   = $_REQUEST["message"];
 
 	$apiKey = getenv("AWS_LAMBDA_KEY");
-	$apiEndpoint = "https://36p03itvai.execute-api.us-east-2.amazonaws.com/default/contactSubmission?service=mixologist&name=".urlencode($name)."&email=".urlencode($email)."&subject=".urlencode($subject)."&message=".urlencode($msg);
+	$apiEndpoint = "https://36p03itvai.execute-api.us-east-2.amazonaws.com/default/contactSubmission?service=mixologist&name=".rawurlencode($name)."&email=".rawurlencode($email)."&subject=".rawurlencode($subject)."&message=".rawurlencode($msg);
 	
-	echo $apiEndpoint;
-	echo "\n";
-	
+
 	//Initialize cURL.
 	$ch = curl_init();
 	// add header for api key
